12f5a7fcb499ea6944938200ab6971198aeaa595
Sjoerd Rutten <sjoerd@ray.nl>
2020-07-27 11:13:23 +0200

    Publish

--- src/LaravelBuckaroo.php
+++ src/LaravelBuckaroo.php
@@ -12,6 +12,34 @@ class LaravelBuckaroo
     {
     }
 
+
+    /**
+     * Generate the authorization header
+     *
+     * @param $requestHttpMethod
+     * @param $requestUri
+     * @param $requestTimestamp
+     * @param $nonce
+     * @param $requestContentBase64String
+     * @return string
+     */
+    private function getAuthorizationHeader($requestHttpMethod, $requestUri, $requestTimestamp, $nonce, $requestContentBase64String): string
+    {
+        $websiteKey = config('laravel_buckaroo.buckaroo_website_key');
+        $secretKey = config('laravel_buckaroo.buckaroo_secret_key');
+
+        $header = [];
+        $header[] = 'hmac '.$websiteKey;
+
+        $data = $websiteKey . $requestHttpMethod . $requestUri . $requestTimestamp . $nonce . $requestContentBase64String;
+        $header[] = hash_hmac('sha256', $data, $secretKey);
+
+        $header[] = uniqid('', true);
+        $header[] = $requestTimestamp;
+
+        return implode(':', $header);
+    }
+
     private function request($methods, $action)
     {
 
